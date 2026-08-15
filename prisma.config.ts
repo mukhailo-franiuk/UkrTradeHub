59e08d662f88b9a29fed60921a51f6d2b8a614e2
mukhailo-franiuk <vipersmf@gmail.com>
2026-08-15 21:12:37 +0300

    New App 4

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+// Перевіряємо наявність реальної змінної, інакше беремо робочу заглушку для стадії білду на Vercel
+const databaseUrl = process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/postgres";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: databaseUrl,
   },
 });
